7a5d6238f2b67df4968f8d1e54b507b2dc1bc1b7
CodeExcalibar <Alex.raju1010@gmail.com>
2024-12-20 13:33:38 +0000

    Added the connect wallet button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
 	return (
-		<div>
-			<h1>Hello world</h1>
-		</div>
+		<header className='flex justify-between items-center h-auto'>
+			<h1 className='font-extrabold mt-[47px] h-39-[px] w-[255px]'>
+				MARKETPLACE.
+			</h1>
+			<button className='w-[203px] h-[67px] border border-black rounded-[87px] px-[42px] py-[24px]'>
+				Connect wallet
+			</button>
+		</header>
 	);
 }
